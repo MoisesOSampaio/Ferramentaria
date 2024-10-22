7418f2b9264895fe88084bf2f08e667413c59242
Moises <moisessampaio19052005@hotmail.com>
2024-10-22 00:39:11 -0300

    adicionando bootstrap ao projeto

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 use SOURCE\routes\Router;
 use Dotenv\Dotenv;
 
@@ -7,7 +7,18 @@ require '../vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__,1));
 $dotenv->load();
+?>
 
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Ferramentaria</title>
+</head>
+<body>
+<?php 
 
 $path = Router::execute();
 
@@ -15,3 +26,7 @@ $path = Router::execute();
 require '../src/view/' . $path;
 
 ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
