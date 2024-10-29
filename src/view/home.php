3c3765dd53fe534c423a72b81329b25714bbfcdf
Moises <moisessampaio19052005@hotmail.com>
2024-10-28 23:48:21 -0300

    mostragem das ferramentas na pagina inicial

--- src/view/home.php
+++ src/view/home.php
@@ -1,2 +1,23 @@
 
 <h1>Home</h1>
+
+
+<?php
+
+use SOURCE\controller\ToolController;
+
+$controller = new ToolController();
+$list = $_SESSION['ToolList']->getList();
+echo "<ul>";
+foreach($list as $tool){
+    echo "<li>" . $tool->getId() . "</li>";
+    echo "<li>" . $tool->getNome() . "</li>";
+    echo "<li>" . $tool->getQuantityStorage() . "</li>";
+    echo "<li>" . $tool->getQuantityDamaged() . "</li>";
+    echo "<li>" . $tool->getQuantityAvailable() . "</li>";
+}
+
+echo "</ul>";
+
+//var_dump($_SESSION['ToolList']->getList());
+?>
